90a0921697848d60d7e87846d9c567a72d7bb314
Kundanzope <95461658+Kundanzope@users.noreply.github.com>
2023-09-16 15:32:55 +0530

    Add files via upload

---
+++ pinote/notes/static/about_us.js
@@ -0,0 +1,10 @@
+let siddhiName = document.querySelector('#siddhi-name')
+let siddhiNo = document.querySelector('#siddhi-no')
+
+function siddhiOn() {
+    siddhiNo.classList.remove('passive')
+}
+
+function siddhiOff() {
+    siddhiNo.classList.add('passive')
+}
